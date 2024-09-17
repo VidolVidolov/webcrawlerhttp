b04d188953cdaf235b6cc7a27df67d33fa797119
Vidol vidolov <vivividolov@gmail.com>
2024-09-17 21:14:21 +0300

    crawl page recursively

--- crawl.js
+++ crawl.js
@@ -1,24 +1,49 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(currnetURL) {
+async function crawlPage(baseURL, currnetURL, pages) {
+  const baseURLObject = new URL(baseURL);
+  const currnetURLObject = new URL(currnetURL);
+
+  if (baseURLObject.hostname !== currnetURLObject.hostname) {
+    return pages;
+  }
+  const normalizedCurrentURL = normalizeURL(currnetURL);
+
+  if (pages[normalizedCurrentURL] > 0) {
+    pages[normalizedCurrentURL]++;
+    return pages;
+  }
+
+  pages[normalizedCurrentURL] = 1;
+
   console.log(`Actively crawling: ${currnetURL}`);
 
   try {
     const resp = await fetch(currnetURL);
+
     if (resp.status > 399) {
       console.log(
         `Error in fetch with status code: ${resp.status} on page: ${currnetURL}`
       );
-      return;
+      return pages;
     }
 
     const contentType = resp.headers.get("content-type");
+
     if (!contentType.includes("text/html")) {
       console.log(
         `No HTML found, content type is ${contentType} on page: ${currnetURL}`
       );
+      return pages;
     }
     const htmlBody = await resp.text();
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
+
+    return pages;
   } catch (error) {
     console.log(`Error in fetch: ${error.message}, on page ${currnetURL}`);
   }
