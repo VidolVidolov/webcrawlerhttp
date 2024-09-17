2f77ab8ebe3d03e1867c2cf38959c31b8daaae23
Vidol vidolov <vivividolov@gmail.com>
2024-09-17 21:31:49 +0300

    print report

---
+++ report.test.js
@@ -0,0 +1,36 @@
+const { sortPages } = require("./report.js");
+const { test, expect } = require("@jest/globals");
+
+describe("report", () => {
+  test("sortPages 2 pages desc", () => {
+    const input = {
+      "https://blog.boot.dev": 3,
+      "https://blog.boot.dev/path": 1,
+    };
+    const actual = sortPages(input);
+    const expected = [
+      ["https://blog.boot.dev", 3],
+      ["https://blog.boot.dev/path", 1],
+    ];
+    expect(actual).toEqual(expected);
+  });
+
+  test("sortPages 5 pages desc", () => {
+    const input = {
+      "https://blog.boot.dev": 3,
+      "https://blog.boot.dev/path": 1,
+      "https://blog.boot.dev/path123": 90,
+      "https://blog.boot.dev/path7": 7,
+      "https://blog.boot.dev/path15": 15,
+    };
+    const actual = sortPages(input);
+    const expected = [
+      ["https://blog.boot.dev/path123", 90],
+      ["https://blog.boot.dev/path15", 15],
+      ["https://blog.boot.dev/path7", 7],
+      ["https://blog.boot.dev", 3],
+      ["https://blog.boot.dev/path", 1],
+    ];
+    expect(actual).toEqual(expected);
+  });
+});
